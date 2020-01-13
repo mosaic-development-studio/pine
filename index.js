68d3537cb3eebd689f3ae84efbc3d1b1d3057328
Mike Skalandunas <mskalandunas@gmail.com>
2020-01-12 23:10:18 -0800

    [express]: render /article view

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 app.use(express.static(__dirname + '/client'));
+
+router.get('/article', (req, res, next) => {
+    res.send('<h1>article</h1>');
+});
+
+app.use('/', router);
 
 function listen(port = 3000) {
     app.listen(port, () => console.log('Listening on ' + port));
